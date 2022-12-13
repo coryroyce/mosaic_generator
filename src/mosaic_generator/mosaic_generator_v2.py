412069ccbf6da1afd2c1eca4255e4d319c68e925
Cory Randolph <coryroyce@gmail.com>
2022-12-12 18:21:00 -0800

    Adding image tile size

--- src/mosaic_generator/mosaic_generator_v2.py
+++ src/mosaic_generator/mosaic_generator_v2.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 import json
-import os
 import math
 import random
 
@@ -8,14 +7,13 @@ import numpy as np
 import cv2
 
 # Set configuration parameters
-SOURCE_IMAGE_PATH = "src/mosaic_generator/source_images/Pikachu.jpeg"
+SOURCE_IMAGE_PATH = "src/mosaic_generator/source_images/B.jpg"  # "src/mosaic_generator/source_images/Pikachu.jpeg"
 TILE_IMAGES_PATH = "src/mosaic_generator/tile_images"
 TILE_IMAGES_CACHE_PATH = "src/mosaic_generator/tile_images_cache/tile_images_cache.json"
+OUTPUT_IMAGE_PATH = "src/mosaic_generator/output_mosaics"
 REGENERATE_NEW_CACHE = False
-NUM_TILES_HEIGHT = 15
-NUM_TILES_WIDTH = 20
-
-# TODO: Need to enlarge the original image to get enough tiles in. Also need to Auto crop the input image based on a standard frame size of how the cards will be layed out
+NUMBER_OF_TILES_HIGH = 21
+NUMBER_OF_TILES_WIDE = 16
 
 
 class ProcessImage:
@@ -23,7 +21,11 @@ class ProcessImage:
         self.source_image_path = SOURCE_IMAGE_PATH
         self.tile_images_path = TILE_IMAGES_PATH
         self.tile_images_cache_path = TILE_IMAGES_CACHE_PATH
+        self.output_image_path = self.create_output_file_name()
         self.regenerate_new_cache = REGENERATE_NEW_CACHE
+        self.number_of_tiles_high = NUMBER_OF_TILES_HIGH
+        self.number_of_tiles_wide = NUMBER_OF_TILES_WIDE
+        self.source_image_original = None
         self.source_image_modified = None
         self.source_image_height = 0
         self.source_image_width = 0
@@ -76,6 +78,13 @@ class ProcessImage:
 
         return closest_color
 
+    def create_output_file_name(self):
+        "Create the output file name based on the image source"
+        input_file_name = Path(self.source_image_path).stem
+        output_file_name = f"{OUTPUT_IMAGE_PATH}/{input_file_name}_Mosaic.jpeg"
+
+        return output_file_name
+
     def check_cache_for_tile_images(self):
         "Check if a cache exists or if should be regenerated based on new tile images"
         if not Path(self.tile_images_cache_path).is_file() or self.regenerate_new_cache:
@@ -110,19 +119,64 @@ class ProcessImage:
 
         return tile_images_data
 
+    def find_nearest_aspect_ratio(self, image):
+        """Take an input image and find the nearest aspect ratio from 9:16, 16:9 or 1:1"""
+        image_original = cv2.imread(self.source_image_path)
+
+        # Aspect ratio is width:height
+        input_aspect_ratio = image_original.shape[1] / image_original.shape[0]
+
+        # 9:16 = 0.5625 AR, 1:1 = 1 AR, 16:9 = 1.7777
+        # Midpoints 0.7813 and 1.3888
+        if input_aspect_ratio <= 0.7813:
+            aspect_ratio = "9:16"
+        elif input_aspect_ratio > 0.7813 and input_aspect_ratio <= 1.3888:
+            aspect_ratio = "1:1"
+        elif input_aspect_ratio >= 1.3888:
+            aspect_ratio = "16:9"
+
+        return aspect_ratio
+
+    @staticmethod
+    def center_crop_image(image):
+        """Returns center cropped image
+        Args:
+        img: image to be center cropped
+        dim: dimensions (width, height) to be cropped
+        """
+        width, height = image.shape[1], image.shape[0]
+
+        # Default to square dimensions can add other aspect ratios later
+        dim = [min(width, height), min(width, height)]
+
+        # Process crop width and height for max available dimension
+        crop_width = dim[0] if dim[0] < image.shape[1] else image.shape[1]
+        crop_height = dim[1] if dim[1] < image.shape[0] else image.shape[0]
+        mid_x, mid_y = int(width / 2), int(height / 2)
+        cw2, ch2 = int(crop_width / 2), int(crop_height / 2)
+        cropped_image = image[mid_y - ch2 : mid_y + ch2, mid_x - cw2 : mid_x + cw2]
+        return cropped_image
+
     def load_and_modify_source_image(self):
         """Load in the source image and then modify based on overall parameters"""
-        image = cv2.imread(self.source_image_path)
-        image_height, image_width, _ = image.shape
+        image_original = cv2.imread(self.source_image_path)
+        self.source_image_original = image_original
 
-        # Calculate the correct number of tiles needed
-        num_tiles_h, num_tiles_w = (
-            image_height // self.tile_height,
-            image_width // self.tile_width,
+        # TODO: Calculate resize factor for just the width
+        # Standard aspect ratio's = 9:16, 16:9 or 1:1
+
+        # Crop the image based on the best aspect ratio
+        image_modified = self.center_crop_image(image_original)
+
+        # Calculate output height and width of image
+        output_image_height = self.number_of_tiles_high * self.tile_height
+        output_image_width = self.number_of_tiles_wide * self.tile_width
+
+        # Resize the image
+        output_dimentions = (output_image_width, output_image_height)
+        image_modified = cv2.resize(
+            image_modified, output_dimentions, interpolation=cv2.INTER_NEAREST
         )
-        image_modified = image[
-            : self.tile_height * num_tiles_h, : self.tile_width * num_tiles_w
-        ]
 
         # Update source image height and width
         self.source_image_height, self.source_image_width, _ = image_modified.shape
@@ -165,10 +219,11 @@ class ProcessImage:
             tile_image = cv2.resize(tile_image, (self.tile_width, self.tile_height))
             self.source_image_modified[y0:y1, x0:x1] = tile_image
 
-            cv2.imshow("Image", self.source_image_modified)
-            cv2.waitKey(1)
+            # # Uncomment to sho image being generated tile by tile
+            # cv2.imshow("Image", self.source_image_modified)
+            # cv2.waitKey(1)
 
-        cv2.imwrite("output.jpg", self.source_image_modified)
+        cv2.imwrite(self.output_image_path, self.source_image_modified)
 
 
 def test_01():
